b939be0c70547c23b07d4b438ffe5afd1ef5c422
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2024-03-25 09:33:50 +0100

    feat: add solution to the task 9

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -177,8 +177,19 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const totalDaysInMonth = new Date(year, month, 0).getDate();
+  let weekendCount = 0;
+
+  for (let day = 1; day <= totalDaysInMonth; day += 1) {
+    const currentDate = new Date(year, month - 1, day);
+    const dayOfWeek = currentDate.getDay();
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      weekendCount += 1;
+    }
+  }
+
+  return weekendCount;
 }
 
 /**
